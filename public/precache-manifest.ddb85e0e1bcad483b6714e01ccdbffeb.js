1493251af53ac8af9480d1293d717eebd57ae49c
Oscar Moreno <oscar.moreno@cti.espol.edu.ec>
2020-01-10 15:15:21 -0500

    finish project

---
+++ public/precache-manifest.ddb85e0e1bcad483b6714e01ccdbffeb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513a27a7423c244ad971",
+    "url": "/css/app.64c4fdcc.css"
+  },
+  {
+    "revision": "428de7c06cf98524b300",
+    "url": "/css/chunk-vendors.c580d68a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d450e4e1cb01442cbb5a52f953620905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2917e4fb3231da94ddc5",
+    "url": "/js/about.71a34826.js"
+  },
+  {
+    "revision": "513a27a7423c244ad971",
+    "url": "/js/app.395542c0.js"
+  },
+  {
+    "revision": "428de7c06cf98524b300",
+    "url": "/js/chunk-vendors.7d7bf5bf.js"
+  },
+  {
+    "revision": "5e053221e9e09e3d5810a6a834452008",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
